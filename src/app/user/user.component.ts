37d6f99562867496b0536155609e284b0d95df6a
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-12 00:25:03 +0330

    use skipSelf decorator

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,14 @@
-import {Component, OnInit, Self} from '@angular/core';
+import {Component, OnInit, SkipSelf} from '@angular/core';
 import {MyCompanyService} from "../my-company.service";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css'],
-  providers: [MyCompanyService]
+  styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
-  constructor(@Self() private myCompanyService: MyCompanyService) { }
+  constructor(@SkipSelf() private myCompanyService: MyCompanyService) { }
 
   ngOnInit(): void {
   }
